e0aa82bc341fbb0e61e4aa1ee7e4e3afe21be4b4
Ben Burwell <bburwell1@gmail.com>
2013-07-31 13:33:48 -0400

    add pepper

---
+++ pepper.js
@@ -0,0 +1,7 @@
+exports.pepper =
+	process.env.MCEMS_PEPPER
+	|| 'h89128h988owa8wd891989hdiawu9d8198nis';
+
+exports.secret =
+	process.env.MCEMS_SESSION_SECRET
+	|| 'j89h28hiuaiusd9u1b2djkbiu2u3budi23udiuad2';
