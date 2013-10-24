bf0e4d30fb777268f21263c17c8081f95ac2c329
Ben Burwell <bburwell1@gmail.com>
2013-10-24 17:46:28 -0400

    Add error checking for empty shifts in API

--- routes/api.js
+++ routes/api.js
@@ -21,17 +21,25 @@ exports.on_duty = function (req, res) {
 		.exec(function (err, shifts) {
 
 			var data = [];
-			shifts.forEach(function (shift) {
-				data.push({
-					name: shift._member.name.first + ' ' + shift._member.name.last,
-					start: shift.start,
-					end: shift.end,
-					phone: shift._member.phone,
-					address: shift._member.campus_address,
-					unit: shift._member.unit
-				});
-			});
+			
+			if (shifts) {
 
-			res.json(data);
+				shifts.forEach(function (shift) {
+					if (shift._member) {
+						data.push({
+							name: shift._member.name.first + ' ' + shift._member.name.last,
+							start: shift.start,
+							end: shift.end,
+							phone: shift._member.phone,
+							address: shift._member.campus_address,
+							unit: shift._member.unit
+						});
+					}
+				});
+
+				res.json(data);
+			} else {
+				res.json([]);
+			}
 	});
 };
